7a2950807727382fd7fdb6d736c68e1ff37bdae8
LanMan <tech@lanmanassociati.com>
2016-10-27 17:20:30 +0200

    Update camera.js

--- www/ios/camera.js
+++ www/ios/camera.js
@@ -1,8 +1,8 @@
-cordova.define("com.bandpad.cordova.microphone.cameraPermission", function(require, exports, module) {
+cordova.define("com.bandpad.cordova.microphone.cameraperm", function(require, exports, module) {
   var exec = require('cordova/exec');
   module.exports = function (next) {
     exec(function (granted) {
       next(granted);
-    }, null, 'Microphone', 'cameraPermission');
+    }, null, 'Microphone', 'cameraperm');
   }; 
 });
